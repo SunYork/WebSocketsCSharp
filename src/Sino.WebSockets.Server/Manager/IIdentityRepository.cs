5a65d74112568ddea0827ec0de494ddd345925b2
姚正发 <976691141@qq.com>
2017-12-27 17:57:10 +0800

    add repository interface and redis class

---
+++ src/Sino.WebSockets.Server/Manager/IIdentityRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sino.WebSockets.Server.Manager
+{
+    /// <summary>
+    /// 提供标识符存储的接口
+    /// </summary>
+    public interface IIdentityRepository
+    {
+
+    }
+}
---
+++ src/Sino.WebSockets.Server/Manager/RedisIdentityRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sino.WebSockets.Server.Manager
+{
+    /// <summary>
+    /// 基于Redis的标识符存储实现
+    /// </summary>
+    internal class RedisIdentityRepository : IIdentityRepository
+    {
+
+    }
+}
