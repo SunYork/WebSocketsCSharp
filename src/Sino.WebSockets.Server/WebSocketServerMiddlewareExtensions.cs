e05739e12438f970b40d70d7f1b99b2691c4ad6f
姚正发 <976691141@qq.com>
2017-12-27 17:41:15 +0800

    add docker support

--- src/Sino.WebSockets.Server/WebSocketServerMiddlewareExtensions.cs
+++ src/Sino.WebSockets.Server/WebSocketServerMiddlewareExtensions.cs
@@ -1,17 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.Extensions.Options;
+using Sino.WebSockets.Server;
+using System;
 
 namespace Microsoft.AspNetCore.Builder
 {
     public static class WebSocketServerMiddlewareExtensions
     {
+        /// <summary>
+        /// 启用WebSocket功能，默认已经启用官方，无需多次启动
+        /// </summary>
         public static IApplicationBuilder UseWebSocketsServer(this IApplicationBuilder app)
         {
-            app.Use(async (context, next) =>
-            {
+            if (app == null)
+                throw new ArgumentNullException(nameof(app));
 
-            });
+            app.UseWebSockets();
+
+            return app.UseMiddleware<WebSocketServerMiddleware>();
+        }
+
+        /// <summary>
+        /// 启用WebSocket功能，默认已经启用官方，无需多次启动
+        /// </summary>
+        public static IApplicationBuilder UseWebSocketsServer(this IApplicationBuilder app, WebSocketServerOptions options)
+        {
+            if (app == null)
+                throw new ArgumentNullException(nameof(app));
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            app.UseWebSockets();
+
+            return app.UseMiddleware<WebSocketServerMiddleware>(Options.Create(options));
         }
     }
 }
--- src/Sino.WebSockets.Server/WebSocketServerOptions.cs
+++ src/Sino.WebSockets.Server/WebSocketServerOptions.cs
@@ -1,14 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Sino.WebSockets.Server
 {
+    /// <summary>
+    /// 配置信息
+    /// </summary>
     public class WebSocketServerOptions
     {
         public WebSocketServerOptions()
         {
-
+            KeepAliveInterval = TimeSpan.FromMinutes(2);
+            ReceiveBufferSize = 4 * 1024;
         }
+
+        /// <summary>
+        /// 心跳检测时间间隔，默认2分钟
+        /// </summary>
+        public TimeSpan KeepAliveInterval { get; set; }
+
+        /// <summary>
+        /// 接收缓存，默认4KB
+        /// </summary>
+        public int ReceiveBufferSize { get; set; }
     }
 }
